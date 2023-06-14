06be84cc4adf03d80b9c24b43b2d5adaf92d84df
potatowon <yewkim0203@gmail.com>
2023-06-14 18:57:35 +0900

    데이터베이스 끝

--- cummerce_board/board/models.py
+++ cummerce_board/board/models.py
@@ -9,7 +9,7 @@ class Item(models.Model):
     id = models.AutoField(primary_key=True)  # 아이템에 대한 pk
     product_name = models.TextField('상품명')
     thumb_img = models.TextField('모델이미지')
-    price = models.IntegerField('가격')
+    price = models.CharField('가격', max_length=225)
     detail_img = models.TextField('상세페이지이미지')
     is_deleted = models.BooleanField(default=False)
 
@@ -21,13 +21,12 @@ class Comment(models.Model):
                                            3, "올바른 이름을 입력해주세요."
                                        )
                                    ])
-    content = models.CharField("댓글", max_length=255,
+    content = models.TextField("댓글",
                                validators=[
                                    validators.MinLengthValidator(
                                        3, "최소 세 글자 이상은 입력해주셔야 합니다."
                                    )
                                ])
-    created_at = models.DateTimeField(auto_now_add=True)
     item = models.ForeignKey('Item',
                              # related_name='comment_set',
                              on_delete=models.SET_NULL, null=True)
