35e617d2b09b328c06d34f7f193e096bcfb447a3
potatowon <yewkim0203@gmail.com>
2023-06-16 13:57:37 +0900

    워드 클라우드 자바스크립트 업데이트

--- cummerce_board/board/views.py
+++ cummerce_board/board/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.urls import reverse
 from .models import Item, Comment
 from .forms import CommentForm
 from django.core.paginator import Paginator
-
+import json
 
 from krwordrank.word import summarize_with_keywords
 
@@ -53,6 +53,31 @@ def board_detail(request, item_id):
 def board_category(request, category_id):
     items = Item.objects.filter(category_id=category_id)
     return render(request, 'board/category.html', {'items': items})
+
+
+def review_dynamic(request, item_id):
+    item = Item.objects.get(id=item_id)
+    all_reviews = item.comment_set.all()
+    texts = []
+    for reviews in all_reviews:
+        texts.append(reviews.content)  # review 테이블에
+    stopwords = {'흠', '너무'}
+    from datetime import datetime
+    start_time = datetime.now()
+    keywords = summarize_with_keywords(texts, min_count=3, max_length=10,  # NLP
+                                       beta=0.85, max_iter=10, stopwords=stopwords, verbose=True)
+    print(len(all_reviews))
+    print(f"종료시간: {datetime.now()-start_time}")
+    wordlist = []
+    count = 0
+    for key, val in keywords.items():  # 다음 라이브러리를 위한 후처리
+        temp = {'name': key, 'value': int(val*10)}
+        wordlist.append(temp)
+        # count += 1
+        # if count >= 30:  # 출력 수 제한
+        #     break
+    print(wordlist)
+    return JsonResponse(wordlist, safe=False)
 
 
 def review_wordcloud(request, item_id):
